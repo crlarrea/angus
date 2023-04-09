c74f03d114d1cd8aa488262e280d39cdfd6d0fd9
q <crlarrea@outlook.com>
2023-04-09 18:35:59 +0100

    added animation to pageNotFound

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
-
+import { Player, Controls } from '@lottiefiles/react-lottie-player';
+import catAnimation from '../assets/img/cat.json'
 export const PageNotFound = () => {
   return (
-    <section>
+    <section className='pageNotFound'>
       <h2>Do you like the look of this demo?</h2>
       <p>Get in touch with us</p>
       <address>
         <a href='mailto:hello@long-lane.co.uk'>hello@long-lane.co.uk</a>
+        <a href='https://www.long-lane.co.uk' target='_blank'>
+          long-lane.co.uk
+        </a>
       </address>
+      <Player autoplay loop speed='0.2' src={catAnimation}>
+        <Controls
+          visible={false}
+          buttons={['play', 'repeat', 'frame', 'debug']}
+          speed='0.5'
+        />
+      </Player>
     </section>
   );
 };
