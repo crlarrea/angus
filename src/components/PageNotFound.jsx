04353cb618cf365f790b0b5252bcc459a5c796e7
Long Lane <tech@long-lane.co.uk>
2023-10-06 10:56:12 +0100

    first commit

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -6,12 +6,6 @@ export const PageNotFound = () => {
     <section className='pageNotFound'>
       <h2>Do you like the look of this demo?</h2>
       <p>Get in touch with us</p>
-      <address>
-        <a href='mailto:hello@long-lane.co.uk'>hello@long-lane.co.uk</a>
-        <a href='https://www.long-lane.co.uk' target='_blank'>
-          long-lane.co.uk
-        </a>
-      </address>
       <Player autoplay loop speed='0.2' src={catAnimation}>
         <Controls
           visible={false}
