4b846172b50b4e99ff732c6274c7f1853d69767a
Long Lane <tech@long-lane.co.uk>
2023-10-06 10:59:02 +0100

    first commit

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -4,8 +4,7 @@ import catAnimation from '../assets/img/cat.json'
 export const PageNotFound = () => {
   return (
     <section className='pageNotFound'>
-      <h2>Do you like the look of this demo?</h2>
-      <p>Get in touch with us</p>
+      <h2>purr...purr</h2>
       <Player autoplay loop speed='0.2' src={catAnimation}>
         <Controls
           visible={false}
