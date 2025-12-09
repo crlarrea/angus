edb5878e7555a0a573771e2d64d19b0a3b0f402d
crlarrea <crlarrea@protonmail.com>
2025-12-09 10:13:33 +0100

    fix animation

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  distDir: "src/build",
 };
 
 export default nextConfig;
