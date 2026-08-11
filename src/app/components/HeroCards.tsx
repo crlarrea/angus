3a5b40e59bdfb23f330fed4d34f7911b30646035
Christian Larrea <crlarrea@protonmail.com>
2026-08-11 17:11:17 +0100

    tweak cards

--- src/app/components/HeroCards.tsx
+++ src/app/components/HeroCards.tsx
@@ -7,19 +7,18 @@ const HeroCards = () => {
   return (
     <div className={styles.heroCards}>
       <div className={styles.happyEverAfters}>
-        {/* <p>Happy Ever Afters</p> */}
+        <p>Happy Ever Afters</p>
         <span>
           <FaPlay />
         </span>
-        
-          {/* <p>Little Susu's story</p> */}
-          <Image
-            src="/susu.avif"
-            fill
-            alt="Susu: Happy Ever Afters."
-            sizes="(max-width:1024px) 80px"
-          />
-        
+        <p>🐈 Little Susu's story</p>
+
+        <Image
+          src="/susu.avif"
+          fill
+          alt="Susu: Happy Ever Afters."
+          sizes="(max-width:1024px) 80px"
+        />
       </div>
       <div className={styles.donationCard}>
         <p>From £2 a month</p>
