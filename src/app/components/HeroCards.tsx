c000f8a1f906bc29d43d53b4b742a1eb50bb89a1
Christian Larrea <crlarrea@protonmail.com>
2026-08-11 17:05:06 +0100

    add happy ever afters

--- src/app/components/HeroCards.tsx
+++ src/app/components/HeroCards.tsx
@@ -1,14 +1,35 @@
 import Link from "next/link";
 import Image from "next/image";
 import styles from "@/app/styles/hero.module.scss";
+import { FaPlay } from "react-icons/fa6";
+
 const HeroCards = () => {
   return (
     <div className={styles.heroCards}>
-      <div className={styles.card}>card 1</div>
-      <div className={styles.card}>
+      <div className={styles.happyEverAfters}>
+        {/* <p>Happy Ever Afters</p> */}
+        <span>
+          <FaPlay />
+        </span>
+        
+          {/* <p>Little Susu's story</p> */}
+          <Image
+            src="/susu.avif"
+            fill
+            alt="Susu: Happy Ever Afters."
+            sizes="(max-width:1024px) 80px"
+          />
+        
+      </div>
+      <div className={styles.donationCard}>
         <p>From £2 a month</p>
         <Link href="donate&amount=2">Let's do it!</Link>
-        <Image src="/cat_with_hat.png" fill sizes="(max-width:1024px) 50px" alt="decorative image"/>
+        <Image
+          src="/cat_with_hat.png"
+          fill
+          sizes="(max-width:1024px) 50px"
+          alt="decorative image"
+        />
       </div>
     </div>
   );
